7b557fd10144cc5ad6a782f7683c8141f2390b0f
Amira de Moya <demoyaamira@gmail.com>
2022-12-14 11:13:35 -0500

    commit tree bdf0f782f39536373845358705a0a651c6700ee0

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+
+   def index
+
+    students = Student.all
+    render json: students
+
+   end
+
+
+   def highest_grade
+
+    byebug
+
+
+    student = Student.order(grade: :desc).first
+    render json: student
+
+   end
+
+
+   def grades
+
+    students = Student.order(grade: :desc)
+    render json: students
+
+   end
+
+   
+
+
+    
+
+end
